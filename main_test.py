4de87de9ab8fde5cd1efa80e46c9b3dfeab17d71
Elias Panula <elias.panula@gmail.com>
2020-03-07 12:24:26 +0200

    MASTER Calculate max sentence length; Add data to .gitignore

--- main_test.py
+++ main_test.py
@@ -1,4 +1,4 @@
-
+import numpy as np
 data_file = "testdata.txt"
 ngram_size = 6
 
@@ -10,7 +10,17 @@ def read_file(filename):
 
 
 def create_unique_words(lines):
+    """
+    Input:
+        lines                   Lines from corpus, one sentence per line
+    Output:
+        unique_words            all unique words in alphabetic order
+        nof_unique_words        length of unique words
+        max_sentence_length     length of the longest sentence
+    """
     unique_words = []
+
+    # Collect unique words from the corpus
     for sentence in lines:
         for word in sentence.split():
             if word not in unique_words:
@@ -18,10 +28,20 @@ def create_unique_words(lines):
 
         unique_words = sorted(unique_words)
 
+    """
+    Add the stop icon to unique words.
+    This will allow our RNN to learn stopping of sentences.
+    In addition, it will allow us to have a fixed size
+    for our sentence tensors.
+    """
     unique_words.append('</s>')
     nof_unique_words = len(unique_words)
 
-    return unique_words, nof_unique_words
+    # Calculate the maximum sentence length used in fixing tensor size
+    np_lines = np.array(lines)
+    max_sentence_length = np.max([len(sentence.split()) for sentence in np_lines])
+
+    return unique_words, nof_unique_words, max_sentence_length
 
 
 def build_index(unique_words):
@@ -32,7 +52,7 @@ def build_index(unique_words):
     return d
 
 lines = read_file(data_file)
-unique_words, nof_unique_words = create_unique_words(lines)
+unique_words, nof_unique_words, n = create_unique_words(lines)
 
 
-print(unique_words, "\n", nof_unique_words)
+print(unique_words, "\n", nof_unique_words, "\n", n)
