3e38fb5784b96f447403feae49017485ffb3c71a
alqio <kyosti.alkio@gmail.com>
2020-04-22 11:41:28 +0300

    Adds easy model and perplexity saving

--- main.py
+++ main.py
@@ -11,15 +11,22 @@ import utils
 from data_helper import SentenceMapper
 from models import LSTM
 
-from itertools import takewhile
-
 import time
 
-data_file = "testdata.txt"
-test_data_file = "testdata.txt" #"actual_testdata_medium.txt"
-save_path = "model_20k.pth"
-perplexity_save_path = "model_20k.pth"
-ngram_size = 6
+data_file_size = 1000
+train_data_file = "testdata_small.txt"
+test_data_file = "testdata_small.txt"
+# model_save_path = "model_20k.pth"
+# perplexity_save_path = "model_20k.pth"
+
+
+def perplexity_save_path(data_file_size, lstm_h_dim, embedding_dim):
+    return "perplexity_" + str(data_file_size / 1000) + "k_" + str(lstm_h_dim) + "_" + str(embedding_dim) + ".csv"
+
+
+def model_save_path(data_file_size, lstm_h_dim, embedding_dim):
+    return "model_" + str(data_file_size / 1000) + "k_" + str(lstm_h_dim) + "_" + str(embedding_dim) + ".pth"
+
 
 cuda = torch.cuda.is_available()
 if cuda:
@@ -37,8 +44,11 @@ def main(load=False):
         print("Loading file", test_data_file, "for testing")
         lines = utils.read_file(test_data_file)
     else:
-        print("Using file", data_file, "for training")
-        lines = utils.read_file(data_file)
+        print("Using file", train_data_file, "for training")
+        lines = utils.read_file(train_data_file)
+
+    global data_file_size
+    data_file_size = len(lines)
 
     start = time.time()
     unique_words, vocab_size, n = utils.create_unique_words(lines)
@@ -49,12 +59,10 @@ def main(load=False):
     mapper = SentenceMapper(lines, word_to_idx, idx_to_word, n)
 
     # Construct dataloader
-    dataset = utils.ReadLines(data_file)
+    dataset = utils.ReadLines(train_data_file)
     loader = torch.utils.data.DataLoader(dataset=dataset, batch_size=16, num_workers=8, shuffle=True)
 
-    # loader = torch.utils.data.DataLoader(tensor, batch_size=hps.batch_size)
-
-    embedding = fasttext.train_unsupervised(data_file, model='cbow', dim=hps.embedding_dim)
+    embedding = fasttext.train_unsupervised(train_data_file, model='cbow', dim=hps.embedding_dim)
 
     # Init model
     if not load:
@@ -65,7 +73,8 @@ def main(load=False):
         print("Loading model...")
         model = LSTM(hps, vocab_size)
         model = nn.DataParallel(model).to(device)
-        model.load_state_dict(torch.load(save_path, map_location=device))
+        model.load_state_dict(
+            torch.load(model_save_path(data_file_size, hps.lstm_h_dim, hps.embedding_dim), map_location=device))
         model.to(device)
         model.eval()
 
@@ -78,10 +87,7 @@ def main(load=False):
             inputs = mapper.map_sentences_to_padded_embedding(input_sequences, embedding=embedding,
                                                               embedding_size=hps.embedding_dim)
             inputs = inputs.to(device)
-            #print("inputs.shape", inputs.shape)
-            #print("inputs", inputs)
             inputs = inputs[0, :].unsqueeze(0)
-            #print("inputs.shape", inputs.shape)
 
             outputs = model(inputs)
 
@@ -91,17 +97,11 @@ def main(load=False):
             output = output.squeeze(2).squeeze(0)
             original_input = inputs.squeeze(0)
 
-            #print(output)
-            #print(output.shape)
-
-            #print('\nOriginal sequence:')
-            input_sequence = input_sequences#[idx_to_word[c] for c in original_input.detach().cpu().numpy()]
-            # input_sequence = list(takewhile(lambda x: x != "</s>", input_sequence))
+            input_sequence = input_sequences
             print(input_sequence)
 
             print('\nPredicted sequence:')
             predicted_sequence = [idx_to_word[c] for c in output.detach().cpu().numpy()]
-            # predicted_sequence = list(takewhile(lambda x: x != "</s>", predicted_sequence))
             print(predicted_sequence)
 
             prev_word = ""
@@ -133,7 +133,7 @@ def train_model(hps, idx_to_word, model, train_loader, validation_loader, mapper
 
     if cuda:
         criterion = criterion.cuda()
-    
+
     print(device, cuda)
 
     optimizer = torch.optim.Adam(model.parameters(), lr=0.01)
@@ -142,8 +142,9 @@ def train_model(hps, idx_to_word, model, train_loader, validation_loader, mapper
     training_loss, validation_loss = [], []
 
     P = pd.DataFrame(
-        {'epoch':list(range(num_epochs)), 'model': save_path, 'perplexity': [0]*num_epochs}
-        )
+        {'epoch': list(range(num_epochs)), 'model': model_save_path(data_file_size, hps.lstm_h_dim, hps.embedding_dim),
+         'perplexity': [0] * num_epochs}
+    )
 
     for i in range(num_epochs):
 
@@ -167,7 +168,8 @@ def train_model(hps, idx_to_word, model, train_loader, validation_loader, mapper
 
             inputs, targets = utils.inputs_and_targets_from_sequences(padded_data)
 
-            inputs = mapper.map_sentences_to_padded_embedding(inputs, embedding=embedding, embedding_size=hps.embedding_dim)
+            inputs = mapper.map_sentences_to_padded_embedding(inputs, embedding=embedding,
+                                                              embedding_size=hps.embedding_dim)
 
             targets = mapper.map_words_to_indices(targets)
 
@@ -176,7 +178,7 @@ def train_model(hps, idx_to_word, model, train_loader, validation_loader, mapper
                 targets = targets.cuda()
 
             # Forward pass
-            
+
             outputs = model(inputs).permute(0, 2, 1)
 
             # Loss
@@ -184,7 +186,7 @@ def train_model(hps, idx_to_word, model, train_loader, validation_loader, mapper
 
             perplexity = torch.exp(loss)
             perplexities.append(perplexity.item())
-            print(perplexity.item())
+
             # Backward pass
             optimizer.zero_grad()
             loss.backward()
@@ -207,7 +209,8 @@ def train_model(hps, idx_to_word, model, train_loader, validation_loader, mapper
             padded_data = mapper.pad_sentences(data)
 
             inputs, targets = utils.inputs_and_targets_from_sequences(padded_data)
-            inputs = mapper.map_sentences_to_padded_embedding(inputs, embedding=embedding,embedding_size=hps.embedding_dim)
+            inputs = mapper.map_sentences_to_padded_embedding(inputs, embedding=embedding,
+                                                              embedding_size=hps.embedding_dim)
             targets = mapper.map_words_to_indices(targets)
 
             if cuda:
@@ -232,17 +235,17 @@ def train_model(hps, idx_to_word, model, train_loader, validation_loader, mapper
             print(
                 f'Epoch {i} took {time.time() - epoch_start}s, training loss: {training_loss[-1]}, validation loss: {validation_loss[-1]}')
 
-    torch.save(model.state_dict(), save_path)
-    P.to_csv(perplexity_save_path, index=False, header=True)
+    torch.save(model.state_dict(), model_save_path(data_file_size, hps.lstm_h_dim, hps.embedding_dim))
+    P.to_csv(perplexity_save_path(data_file_size, hps.lstm_h_dim, hps.embedding_dim), index=False, header=True)
 
 
 def init_hps():
     parser = argparse.ArgumentParser()
 
-    parser.add_argument("--lstm_h_dim", type=int, default=200,
+    parser.add_argument("--lstm_h_dim", type=int, default=600,
                         help="dimension of the hidden layer for lstm")
 
-    parser.add_argument("--embedding_dim", type=int, default=100,
+    parser.add_argument("--embedding_dim", type=int, default=200,
                         help="dimension of the embedding")
 
     parser.add_argument("--batch_size", type=int, default=16,
